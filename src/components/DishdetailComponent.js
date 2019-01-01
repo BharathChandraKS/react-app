29dba145ae47044c9891e8192f9cc26ed945691d
Bharath Chandra <bharathchandraks@gmail.com>
2019-01-02 01:55:34 +1100

    Added new component to render dish details with comments

---
+++ src/components/DishdetailComponent.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import {
+    Card, CardImg, CardImgOverlay, CardText, CardBody,
+    CardTitle
+} from 'reactstrap';
+
+class Dishdetail extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    formatDate(commentDate) {
+        const monthNames = ["January", "February", "March", "April", "May", "June",
+            "July", "August", "September", "October", "November", "December"];
+        var date = new Date(commentDate);
+        var format = `${monthNames[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`;
+        return format;
+    }
+
+
+    renderComments() {
+        if (this.props.dish.comments != null) {
+            const comments = this.props.dish.comments.map((comment) => {
+                return (
+                    <Card key={comment.id}>
+                        <CardText>{comment.comment}</CardText>
+                        <CardText>-- {comment.author}, {this.formatDate(comment.date)}</CardText>
+                    </Card>
+                );
+            });
+            return comments;
+        } else {
+            return (<div></div>);
+        }
+    }
+
+    render() {
+        return (
+            <div className="container" >
+                <div className="row">
+                    <div className="col-12 col-md-5 m-1">
+                        <Card>
+                            <CardImg top src={this.props.dish.image} alt={this.props.dish.name} />
+                            <CardBody>
+                                <CardTitle>{this.props.dish.name}</CardTitle>
+                                <CardText>{this.props.dish.description}</CardText>
+                            </CardBody>
+                        </Card>
+                    </div>
+                    <div className="col-12 col-md-5 m-1">
+                        <Card>
+                            <h4>Comments</h4>
+                            <CardText>
+                                {this.renderComments()}
+                            </CardText>
+                        </Card>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Dishdetail;
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -4,9 +4,9 @@ import {
   Card, CardImg, CardImgOverlay, CardText, CardBody,
   CardTitle
 } from 'reactstrap';
+import Dishdetail from './DishdetailComponent';
 
 class Menu extends Component {
-
   constructor(props) {
     super(props);
 
@@ -17,23 +17,6 @@ class Menu extends Component {
 
   onDishSelect(dish) {
     this.setState({ selectedDish: dish });
-  }
-
-  renderDish(dish) {
-    if (dish != null)
-      return (
-        <Card>
-          <CardImg top src={dish.image} alt={dish.name} />
-          <CardBody>
-            <CardTitle>{dish.name}</CardTitle>
-            <CardText>{dish.description}</CardText>
-          </CardBody>
-        </Card>
-      );
-    else
-      return (
-        <div></div>
-      );
   }
 
   render() {
@@ -51,18 +34,27 @@ class Menu extends Component {
       );
     });
 
-    return (
-      <div className="container">
-        <div className="row">
-          {menu}
-        </div>
-        <div className="row">
-          <div className="col-12 col-md-5 m-1">
-            {this.renderDish(this.state.selectedDish)}
+    if (this.state.selectedDish != null) {
+      return (
+        <div className="container">
+          <div className="row">
+            {menu}
+          </div>
+          <div className="row">
+            <Dishdetail dish={this.state.selectedDish} />
           </div>
         </div>
-      </div>
-    );
+      );
+    }
+    else {
+      return (
+        <div className="container">
+          <div className="row">
+            {menu}
+          </div>
+        </div>
+      );
+    }
   }
 }
 
